82a3fc5471877a4e77c035f3ec376cc64877c536
Benyamin Bashari <benyamin.bashari@ucalgary.ca>
2020-01-22 12:43:41 -0700

    319 Week 1 updated, 413 Week 1 added

---
+++ documents/319/Week1/Employee.java
@@ -0,0 +1,31 @@
+public class Employee {
+    private int hours, vacationDays;
+    private double salary;
+
+    public Employee() {
+        hours = 40;
+        vacationDays = 20;
+        salary = 100000.00;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public int getVacationDays() {
+        return vacationDays;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "hours=" + hours +
+                ", vacationDays=" + vacationDays +
+                ", salary=" + salary +
+                '}';
+    }
+}
---
+++ documents/319/Week1/Manager.java
@@ -0,0 +1,26 @@
+public class Manager extends Employee {
+    private int vacationDays;
+
+    public Manager() {
+        super();
+        vacationDays = super.getVacationDays() + 10;
+    }
+
+    @Override
+    public double getSalary() {
+        return super.getSalary() * 3;
+    }
+
+    public void doManagerStuff() {
+        System.out.println("Doing manager stuff");
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "hours=" + super.getHours() +
+                ", vacationDays=" + vacationDays +
+                ", salary=" + getSalary() +
+                '}';
+    }
+}
